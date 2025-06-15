daaaede66bf7a72ec52ad09be91f3cd2a243323c
Serhii Zadera <serhii.zadera@guesty.com>
2025-06-15 08:33:28 +0200

    HW_2

---
+++ internal/db/db.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("ALERT .env file not found or not loaded")
+	}
+}
+
+func Connect() (*sql.DB, error) {
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	return sql.Open("postgres", connStr)
+}
